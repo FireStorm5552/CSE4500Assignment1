fee250a4c3dff2d716b4ec68101c99f8e42c1541
Nathan Anderson <nathanranderson5973@gmail.com>
2022-02-16 02:50:10 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
-<?
-   $name = "Nathan Anderson";
-   echo "Hello ".$name;
+<?php
+
+// Read the JSON file
+$json_file = file_get_contents('my_data.json');
+
+// Decode the JSON file
+$json_data = json_decode($json_file,true);
+
 ?>
+
+<html>
+   <!-- Name --!>
+<h1>
+    <?php echo $json_data['first-name']; ?>
+    <span class="text-primary"><?php echo $json_data['last-name']; ?></span>
+</h1>
+<?php foreach($json_data['experience'] AS $experience) { ?>
+  <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+    <div class="flex-grow-1">
+      <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
+      <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
+      <p><?php echo $experience['description']; ?></p>
+    </div>
+    <div class="flex-shrink-0">
+      <span class="text-primary"><?php echo $experience['period']; ?></span>
+    </div>
+  </div>
+<?php  } ?>
+</html>
